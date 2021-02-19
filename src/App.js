cd23c4ea4502cc6332d63bc3f88919f7c79070ba
milad-raza <miladrazabhatti@gmail.com>
2021-02-19 15:27:29 +0500

    Minor bug fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MobileComponent from './components/MobileComponent'
 
 function App() {
   return (
-    <div className={App}>
+    <div className="App">
       <MobileComponent />
     </div>
   );
